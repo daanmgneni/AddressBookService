8a47d476abc82f8e57d68d5a17e9a844264ea303
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-29 08:32:36 +0530

    Update AddressBookService.sql

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,6 +1,7 @@
 /*!!!! WELCOME TO ADDRESS BOOK SERVICE !!!! */
 
-  CREATE TABLE address_book (
+  CREATE TABLE address_book /* create table */
+  (
   id INT PRIMARY KEY IDENTITY,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
@@ -12,4 +13,9 @@
   email VARCHAR(100) NOT NULL
 )
 
-SELECT * FROM address_book
+SELECT * FROM address_book /* retrive data */
+
+/* insert values */
+INSERT INTO address_book VALUES('Dhanesh','P','Sector-7','Bhilai','CG','490006','908765323','daan@gmail.com'),
+								('Divya','Mishra','Block2','Lucknow','UP','490566','90845723','divya@gmail.com'),
+								('Shivam','Tripathi','Area -8/11','Bhopal','MP','370006','903456323','shivam@gmail.com')
