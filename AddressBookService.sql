57aea192d7ade5b5aa572ef5303f99e74c28deea
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-29 08:29:37 +0530

    Update AddressBookService.sql

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,7 +1,15 @@
 /*!!!! WELCOME TO ADDRESS BOOK SERVICE !!!! */
 
-   CREATE DATABASE  address_book_service
+  CREATE TABLE address_book (
+  id INT PRIMARY KEY IDENTITY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NOT NULL,
+  zip VARCHAR(20) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(100) NOT NULL
+)
 
-   SELECT NAME FROM SYS.DATABASES
-
-   USE  address_book_service
+SELECT * FROM address_book
